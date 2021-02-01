956f936166c22de6b24f523822fd336aec49d515
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-01 21:22:29 +0100

    added design to main page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,8 +7,14 @@ const Home = () => {
   return (
   <div className="main__mainContainer">
     <div className={"home__image"}><img src="https://www.semmelweiskiado.hu/images/termekek/2/1691/semmelweis_kiado_logo_1608213956.jpg" alt="logo"/></div>
-    <a className={'main__loginButton'} href="/login">Login</a>
-    <a className={'main__registerButton'} href="/register">Register</a>
+    <div className="main__LoginContainer">
+      <form >
+        <input placeholder={"username"} type="text" className="main__usernameInput"/>
+        <input placeholder={"password"} type="password" className="main__passwordInput"/>
+      </form>
+    </div>
+    <button className={'main__loginButton'} >Login</button>
+    <button  className={'main__registerButton'}>Register</button>
   </div>
   );
 };
