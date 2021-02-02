f5d484667a24e52e55851a11edc9c8b81bf34f39
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-02 17:07:23 +0100

    added base body and route

---
+++ src/pages/studentStatistics/StudentStatistics.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const StudentStatistics = () => {
+    return <div className="student__statisticsMain">
+        <h1>test</h1>
+    </div>
+}
+
+export default StudentStatistics;
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from "./pages/home/Home";
 import Student from "./pages/student/Student";
+import StudentStatistics from "./pages/studentStatistics/StudentStatistics";
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/student" component={Student} />
-      <Route exact path="/student/statistics" component={Student} />
+      <Route exact path="/student/statistics" component={StudentStatistics} />
     </Switch>
   </Router>
 );
