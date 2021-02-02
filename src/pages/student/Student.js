02e4ed5b073a0f5e7ae32c24d36b6ca21badddea
Maryn Zolnai <zolnaimaryn1421@gmail.com>
2021-02-02 12:58:35 +0100

    added font awesome

--- src/pages/student/Student.js
+++ src/pages/student/Student.js
@@ -3,8 +3,14 @@ import './Student.css'
 
 
 const Student = () => {
+    const name = "test name"
     return <div className="student__mainContainer">
-
+        <div className="student__header">
+            <p className={"student__welcome"}>welcome</p>
+            <p className="student__name">{name}</p>
+            <span className="student__icon">
+            </span>
+        </div>
     </div>
 }
 
