1e390b37589d3d87a96805246bc2c24980b171f9
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-02 14:01:44 +0100

    fixed main page design for small phones

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Routes from './routes';
 import React from "react";
 
 function App() {
-    const url = process.env.REACT_APP_URL;
+   // const url = process.env.REACT_APP_URL;
 
     return (
     <div className="App">
