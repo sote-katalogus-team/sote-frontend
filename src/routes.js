bd001af8aaaf5abf5a0bd9958ab9f8d63a10ee3b
Adam Hamar <adam.hamar@gmail.com>
2021-02-02 18:04:02 +0100

    Modify routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/signup" component={Signup} />
     </Switch>
   </Router>
 );
