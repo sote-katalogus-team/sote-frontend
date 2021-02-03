ae3c205d6e030d04f224bcb264b544ecc7ef04df
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-03 12:17:48 +0100

    added inputs and selects

--- src/components/newLesson/NewLesson.js
+++ src/components/newLesson/NewLesson.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 const NewLesson = () => {
-    return <div className="newLesson__main">
+    const url = process.env.REACT_APP_URL;
+    const [turns, setTurns] = useState([])
 
+
+    useEffect(() => {
+        fetchTurns();
+    },[])
+
+
+    async function  fetchTurns() {
+        axios.get(url+ "/turnus/all").then((res) => {
+            setTurns(res.data)
+        })
+    }
+
+    return <div className="newLesson__main">
+        <div className="newLesson__selectContainer">
+            <select name="turn" id="1" className="newLesson__turnSelect">
+                {turns.map(turn => (
+                    <option value={turn.id} className="turn__option">{turn.name}</option>
+                ))}
+            </select>
+            <select name="type" id="2" className="newLesson__lessonType">
+                <option value="eloadas" className="type__option">Elöadás</option>
+                <option value="gyakorlat" className="type__option">Gyakorlat</option>
+                <option value="konzultacio" className="type__option">Konzultáció</option>
+            </select>
+
+            <input type="text" className="name__input"/>
+
+            <input type="number" min={1} max={3} className={"number__input"}/>
+
+            <input type="date"/>
+
+        </div>
     </div>
 }
 
--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './Admin.css'
 import AdminSidebar from "../../components/adminSidebar/AdminSidebar";
+import NewLesson from "../../components/newLesson/NewLesson";
 
 
 const Admin = () => {
     return <div className="admin__main">
         <AdminSidebar />
+        <NewLesson />
     </div>
 }
 
