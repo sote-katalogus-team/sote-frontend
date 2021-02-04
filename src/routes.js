a278542e0f73d9d60c717c20b16ed740fcc53d4e
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-04 17:12:30 +0100

    added new turnus page to router

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Home from "./pages/home/Home";
 import Student from "./pages/student/Student";
 import StudentStatistics from "./pages/studentStatistics/StudentStatistics";
 import AdminNewLesson from "./pages/admin/AdminNewLesson";
+import AdminNewTurnus from "./pages/admin/AdminNewTurnus";
 
 const Routes = () => (
   <Router>
@@ -14,6 +15,7 @@ const Routes = () => (
       <Route exact path="/student" component={Student} />
       <Route exact path="/student/statistics" component={StudentStatistics} />
       <Route exact path="/admin/new-lesson" component={AdminNewLesson} />
+      <Route exact path="/admin/new-turnus" component={AdminNewTurnus} />
     </Switch>
   </Router>
 );
