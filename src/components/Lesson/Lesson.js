01d07425cc726b75c0d9e5aeddaf33f363572744
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-04 10:15:47 +0100

    added a base table

--- src/components/Lesson/Lesson.js
+++ src/components/Lesson/Lesson.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import fontawesome from '@fortawesome/fontawesome'
+import {faTrash} from '@fortawesome/fontawesome-free-solid'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Lesson = (props) => {
+
+    fontawesome.library.add(faTrash);
+
+
+    let potlasText = "nem"
+    if (props.data.potas) {
+        potlasText = "igen"
+    }
+
+    let statuszText = "aktív";
+
+    if (!props.data.active) {
+        statuszText = "passzív"
+    }
+
     return <tr>
-        <td>name</td>
+        <td>{props?.type}</td>
+        <td>{props.data?.name}</td>
+        <td>{props.data?.date}</td>
+        <td>{props.data?.point}</td>
+        <td>{potlasText}</td>
+        <td>{statuszText}</td>
+        <td> <FontAwesomeIcon icon="trash"  className={"lesson_trash)"}/></td>
     </tr>
 }
 
--- src/components/lessonLister/LessonLister.js
+++ src/components/lessonLister/LessonLister.js
@@ -4,23 +4,26 @@ import Lesson from "../Lesson/Lesson";
 
 
 const LessonLister = () => {
-    const[lessons, setLessons] = useState([])
-    const[turnusId, setTurnusId] = useState(1);
-    const[turns, setTurns] = useState([]);
+    //  const[lessons, setLessons] = useState([])
+    const [konz, setKonz] = useState([])
+    const [elo, setElo] = useState([])
+    const [gyak, setGyak] = useState([])
+    const [turnusId, setTurnusId] = useState(1);
+    const [turns, setTurns] = useState([]);
     const url = process.env.REACT_APP_URL;
 
 
     useEffect(() => {
         fetchTurns();
-    },[])
+    }, [])
 
     useEffect(() => {
         getClassesById()
-    },[turnusId])
+    }, [turnusId])
 
 
-    async function  fetchTurns() {
-        axios.get(url+ "/turnus/all").then((res) => {
+    async function fetchTurns() {
+        axios.get(url + "/turnus/all").then((res) => {
 
 
             setTurns(res.data)
@@ -30,20 +33,15 @@ const LessonLister = () => {
 
     async function getClassesById() {
         axios.get(url + "/classes/all/" + turnusId).then(res => {
-
-            let all = [];
-            Object.keys(res.data).forEach(elem => {
-                res.data[elem].forEach(item => {
-                    all.push(item)}
-                )})
-            setLessons(all)
+            let all = Object.keys(res.data);
+            setElo(res.data[all[0]])
+            setGyak(res.data[all[1]])
+            setKonz(res.data[all[2]])
         })
     }
 
 
-    console.log(lessons)
-
-
+    console.log(elo)
 
 
     return <div className="lessons__main">
@@ -54,24 +52,31 @@ const LessonLister = () => {
         </select>
         <div className="lessons__tableContainer">
             <table>
-
+                <thead>
                 <th>Óra típusa</th>
                 <th>Óra neve</th>
                 <th>Dátuma</th>
                 <th>Értéke</th>
-                <th>kötelezö jelenlét</th>
                 <th>pótlás</th>
                 <th>státusz</th>
                 <th>törlés</th>
-                <Lesson name={"test"} />
-                <Lesson name={"test"} />
-                <Lesson name={"test"} />
-                <Lesson name={"test"} />
+                </thead>
+                <tbody>
+                {elo.map(e => (
+                    <Lesson type={"elöadás"} data={e} />
+                ))}
+                {gyak.map(e => (
+                    <Lesson type={"gyakorlat"} data={e} />
+                ))}
+                {konz.map(e => (
+                    <Lesson type={"konzultáció"} data={e} />
+                ))}
+
+                </tbody>
+
             </table>
         </div>
     </div>
-
-
 
 
 }
