477d35dd292d4e6d932208ceb020bc0d93c47eed
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-04 17:09:55 +0100

    added a base body to the AdminNewTurnus page

---
+++ src/pages/admin/AdminNewTurnus.js
@@ -0,0 +1,18 @@
+import React from "react";
+import AdminSidebar from "../../components/adminSidebar/AdminSidebar";
+import NewLesson from "../../components/newLesson/NewLesson";
+import LessonLister from "../../components/lessonLister/LessonLister";
+import NewTurnus from "../../components/newTurnus/NewTurnus";
+
+const AdminNewTurnus = () => {
+    return <div className="admin__main">
+        <div className="admin__head">
+            <AdminSidebar />
+            <NewTurnus />
+        </div>
+        <LessonLister />
+
+    </div>
+}
+
+export default  AdminNewTurnus;
