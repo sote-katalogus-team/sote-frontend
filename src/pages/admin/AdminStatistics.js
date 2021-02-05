5cd018e1fea81b813d5f18864e42100a2db72282
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-05 14:55:11 +0100

    added StatisticsTable component

--- src/pages/admin/AdminStatistics.js
+++ src/pages/admin/AdminStatistics.js
@@ -2,12 +2,13 @@ import React from "react";
 import AdminSidebar from "../../components/adminSidebar/AdminSidebar";
 import NewTeacher from "../../components/newTeacher/NewTeacher";
 import TeacherLister from "../../components/teacherLister/TeacherLister";
+import StatisticsFilter from "../../components/statisticsFilter/StatisticsFilter";
 
 const AdminStatistics = () => {
     return <div className="admin__main">
         <div className="admin__head">
             <AdminSidebar active={"new-turnus"} />
-            <NewTeacher />
+            <StatisticsFilter />
         </div>
         <TeacherLister />
 
