de50e6b2659cc2850acc1c2d51296d8de956f7eb
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-05 14:16:48 +0100

    finished teacher deleting

--- src/components/teacher/Teacher.js
+++ src/components/teacher/Teacher.js
@@ -1,11 +1,32 @@
 import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import axios from "axios";
 
 const Teacher = (props) => {
+    const url = process.env.REACT_APP_URL;
+
+
+    const deleteTeacher = () => {
+        let ok = window.confirm("Biztosan törölni akarod " + props.data?.name + " nevu oktatot??")
+        if (ok === false) {
+            return
+        }
+        deleteTheTeacher()
+    }
+
+
+    async function deleteTheTeacher() {
+        axios.delete(url + "/teacher/" + props.data?.id +  "/delete").then(res => {
+            alert(res.data)
+            window.location.reload();
+        })
+    }
+
+
     return <tr>
             <td>{props.data?.name}</td>
             <td>{props.data?.email}</td>
-            <td> <FontAwesomeIcon  icon="trash"  className={"lesson_trash)"}/></td>
+            <td  onClick={deleteTeacher} > <FontAwesomeIcon  icon="trash"  className={"lesson_trash)"}/></td>
         </tr>
 }
 
