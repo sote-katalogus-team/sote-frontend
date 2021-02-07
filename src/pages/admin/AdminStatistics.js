e1e294ff993cb71b33e0fa4eace7584eb599d9c9
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-07 12:35:05 +0100

    finished statistics

--- src/pages/admin/AdminStatistics.js
+++ src/pages/admin/AdminStatistics.js
@@ -4,6 +4,7 @@ import StatisticsFilter from "../../components/statisticsFilter/StatisticsFilter
 import StatisticsTable from "../../components/statisticsTable/StatisticsTable";
 
 const AdminStatistics = () => {
+    //wau
     return <div className="admin__main">
         <div className="admin__head">
             <AdminSidebar active={"new-turnus"} />
