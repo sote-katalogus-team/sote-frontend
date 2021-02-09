49c67f4236fff4369b56285eee77b12973c22a8e
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-09 14:08:24 +0100

    de statistics are the students statistics

--- src/pages/studentStatistics/StudentStatistics.js
+++ src/pages/studentStatistics/StudentStatistics.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './StudentStatistics.css'
 import Diagram from "../../components/diagram/Diagram";
+import axios from "axios";
 
 const StudentStatistics = () => {
-    const stat1 = 100;
-    const stat2 = 40;
-    const stat3 = 75;
+    const [student, setStudent] = useState(1)
+    const [stats, setStats] = useState([])
+    const url = process.env.REACT_APP_URL;
+
 
     const back = () => {
         window.location = "/student"
     }
 
-    return <div className="student__statisticsMain">
-        <p className="student__title">Practice:</p>
-        <Diagram stat={stat1} />
-        <p className="student__title">Lecture:</p>
-        <Diagram stat={stat2} />
-        <p className="student__title">Consultation:</p>
-        <Diagram stat={stat3} />
-        <div className="student__statisticsButtonContainer">
-            <button onClick={back} className="student__back">back</button>
-        </div>
+    useEffect(() => {
+        fetchStudentsStats();
+    }, [])
 
+
+
+    async  function fetchStudentsStats() {
+        axios.get(url + "/student/" + student + "/statistics").then(res => {
+            setStats(res.data)
+        })
+    }
+
+
+    let content = <div className="loading">
+        <h1>LOADING</h1>
     </div>
+
+
+    if (stats.length !== 0) {
+        content = <div className="student__statisticsMain">
+            <p className="student__title">Practice:</p>
+            <Diagram stat={stats.percentages?.practice} practice={true} />
+            <p className="student__title">Lecture:</p>
+            <Diagram stat={stats.percentages?.lecture} practice={false} />
+            <p className="student__title">Consultation:</p>
+            <Diagram stat={stats.percentages?.consultation} practice={false} />
+            <div className="student__statisticsButtonContainer">
+                <button onClick={back} className="student__back">back</button>
+            </div>
+
+        </div>
+    }
+
+
+    return content
 }
 
 export default StudentStatistics;
