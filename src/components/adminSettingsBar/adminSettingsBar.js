c3fc73189fa7de5b858604f601bcf48e12d65683
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-11 15:00:26 +0100

    added adminSettings component to the AdminSettings page

--- src/components/adminSettingsBar/adminSettingsBar.js
+++ src/components/adminSettingsBar/adminSettingsBar.js
@@ -2,7 +2,9 @@ import React from "react";
 
 
 const AdminSettingsBar = () => {
+    return <div className="adminSettingsMain">
 
+    </div>
 }
 
 export default AdminSettingsBar;
--- src/pages/admin/AdminSettings.js
+++ src/pages/admin/AdminSettings.js
@@ -1,17 +1,13 @@
 import React from "react";
 import AdminSidebar from "../../components/adminSidebar/AdminSidebar";
-import NewLesson from "../../components/newLesson/NewLesson";
-import LessonLister from "../../components/lessonLister/LessonLister";
 
 
 const AdminSettings = () => {
     return <div className="admin__main">
         <div className="admin__head">
             <AdminSidebar active={"new-lesson"} />
-            <NewLesson />
+            <AdminSettings />
         </div>
-        <LessonLister />
-
     </div>
 }
 
