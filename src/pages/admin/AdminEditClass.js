5f185f12ad6465e4ef977f92bfccd5da4df992b0
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-12 09:56:58 +0100

    added page to admin pages

---
+++ src/pages/admin/AdminEditClass.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const AdminEditClass = () => {
+   return  <div className="adminEdit__mainContainer">
+       <h1>hello</h1>
+   </div>
+}
+
+
+
+export default AdminEditClass;
