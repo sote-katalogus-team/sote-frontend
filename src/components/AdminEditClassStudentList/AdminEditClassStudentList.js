07eb55887c6f3b49bf8b7e86a244127d2d36cd74
maryn1421 <zolnaimaryn1421@gmail.com>
2021-02-15 10:20:42 +0100

    added an snackbar response, with shows us the result of adding a student by neptunCode

--- src/components/AdminEditClassStudentList/AdminEditClassStudentList.js
+++ src/components/AdminEditClassStudentList/AdminEditClassStudentList.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import {
     Button,
     makeStyles,
-    Paper,
+    Paper, Snackbar,
     Table,
     TableBody,
     TableCell,
@@ -14,6 +14,7 @@ import {
 import axios from "axios";
 import authHeader from "../../security/auth-header";
 import {useCookies} from "react-cookie";
+import {Alert} from "@material-ui/lab";
 
 const useStyles = makeStyles({
     table: {
@@ -35,18 +36,16 @@ const AdminEditClassStudentList = (props) => {
     const url = process.env.REACT_APP_URL;
     const [lessonData, setLessonData] = useState([])
     const [finished, setFinished] = useState(false)
+    const [snackVisible, setSnackVisible] = useState(false)
+    const [snackType, setSnackType] = useState("error")
     const classes = useStyles();
-
     const theme = useTheme();
 
-
     let content = ""
-
-
+    let addingAlert = ""
 
 
     useEffect(() => {
-        console.log("halika")
         fetchLessonStudent().then(res => {
             setLessonData(res)
             setFinished(true)
@@ -54,20 +53,36 @@ const AdminEditClassStudentList = (props) => {
     }, [])
 
 
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setSnackVisible(false);
+    };
 
 
-
-
-
+    if (snackVisible) {
+        if (snackType === "error") {
+            addingAlert = <Snackbar autoHideDuration={6000} open={snackVisible}>
+                <Alert onClose={handleClose} severity="error">
+                    Hozzáadás sikertelen!
+                </Alert>
+            </Snackbar>
+        } else {
+            addingAlert = <Snackbar autoHideDuration={6000} open={snackVisible}>
+                <Alert onClose={handleClose} severity="success">
+                    Hozzáadás sikeres!
+                </Alert>
+            </Snackbar>
+        }
+    }
 
 
     async function fetchLessonStudent() {
-        try  {
-            const resp = await axios.get(url + "/"+ props.type + "/"+ props.data + "/students", {headers: authHeader(cookies.user)});
-            console.log(resp.data)
+        try {
+            const resp = await axios.get(url + "/" + props.type + "/" + props.data + "/students", {headers: authHeader(cookies.user)});
             return (resp.data);
-        }
-        catch (error)  {
+        } catch (error) {
             console.log(error)
         }
     }
@@ -80,17 +95,21 @@ const AdminEditClassStudentList = (props) => {
         e.preventDefault()
         saveStudent(document.getElementById("neptunCode").value).then(res => {
             fetchLessonStudent().then(res => {
-                setLessonData(res)
+                    setLessonData(res)
                 }
             )
+            if (res !== undefined) {
+                setSnackType("success")
+                setSnackVisible(true)
+
+            }
+        }).catch(() => {
         })
-
-
 
 
     }
 
-    async function saveStudent (neptunCode) {
+    async function saveStudent(neptunCode) {
         try {
             const resp = await axios.post(url + "/student/addByNeptunCode", {
                 "neptunCode": neptunCode,
@@ -100,22 +119,21 @@ const AdminEditClassStudentList = (props) => {
                 }
             }, {headers: authHeader(cookies.user)})
             return resp.data
-        }
-        catch (error) {
-            console.log(error)
+        } catch (error) {
+            setSnackType("error")
+            setSnackVisible(true)
         }
     }
 
 
     if (finished === true) {
         if (lessonData !== undefined) {
-
             if (props?.data && props.type) {
                 console.log(lessonData)
                 content = <div className="studentList__container">
                     <div className={theme.root}>
-                        <form onSubmit={submitNewStudent} >
-                            <TextField required={true} id="neptunCode" label="neptun kód" variant="outlined" />
+                        <form onSubmit={submitNewStudent}>
+                            <TextField required={true} id="neptunCode" label="neptun kód" variant="outlined"/>
                             <Button className={"addStudentButton"} type={"submit"} variant="outlined" color="primary">
                                 Hozzáadás
                             </Button>
@@ -144,11 +162,13 @@ const AdminEditClassStudentList = (props) => {
                             </TableBody>
                         </Table>
                     </TableContainer>
+                    {
+                        addingAlert
+                    }
                 </div>
             }
         }
     }
-
     return content
 }
 
