c340dd7873c7e9709f2fd9d087dc39d00612155c
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-02-18 11:29:05 +0100

    fixed date sending bug

--- src/components/adminImportTurn/AdminImportTurnus.js
+++ src/components/adminImportTurn/AdminImportTurnus.js
@@ -5,12 +5,16 @@ import {useCookies} from "react-cookie";
 import fontawesome from '@fortawesome/fontawesome'
 import {faArrowAltCircleDown} from '@fortawesome/fontawesome-free-solid'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import ImportLesson from "../importLesson/ImportLesson";
 
 const AdminImportTurnus = () => {
     const [turns, setTurns] = useState([])
     const url = process.env.REACT_APP_URL;
     const [cookies, setCookies] = useCookies(["user"])
     const [lessons, setLessons] = useState([])
+    const [konz, setKonz] = useState([])
+    const [elo, setElo] = useState([])
+    const [gyak, setGyak] = useState([])
 
     fontawesome.library.add(faArrowAltCircleDown);
 
@@ -22,14 +26,49 @@ const AdminImportTurnus = () => {
     }, [])
 
 
-
     async function fetchTurns() {
         try {
-            const resp = await  axios.get(url + "/turnus/all", {headers: authHeader(cookies.user)})
+            const resp = await axios.get(url + "/turnus/all", {headers: authHeader(cookies.user)})
             return resp.data
-        }
-        catch (error) {
+        } catch (error) {
 
+        }
+    }
+
+    async function importLessons(turn1, turn2) {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    let tableBody = <tbody> </tbody>
+
+    if (lessons !== null && lessons !== undefined) {
+        console.log(lessons, "LESSSSONS")
+        tableBody =
+            <tbody>
+            { konz.map(lesson => (
+            <ImportLesson data={lesson} type={"konzultáció"}/>
+            ))}
+            {gyak.map(lesson => (
+            <ImportLesson data={lesson} type={"gyakorlat"}/>
+            ))}
+            {elo.map(lesson => (
+            <ImportLesson data={lesson} type={"eloadas"}/>
+            ))}
+            </tbody>
+
+    }
+
+
+    async function fetchLessons(turnId) {
+        try {
+            const resp = await axios.get(url + "/classes/all/" + turnId, {headers: authHeader(cookies.user)});
+            return resp.data
+        } catch (error) {
+            console.log(error)
         }
     }
 
@@ -41,6 +80,16 @@ const AdminImportTurnus = () => {
 
     }
 
+    function selectListTurn(e) {
+        fetchLessons(e.target.value).then(res => {
+            setLessons(res)
+            let all = Object.keys(res);
+            setElo(res[all[0]])
+            setGyak(res[all[1]])
+            setKonz(res[all[2]])
+        })
+    }
+
     return <div className="importTurnus__main">
         <div className="selectContainers">
             <p>Válassza ki a Turnust amelynek az óráit szeretné másolni:</p>
@@ -49,7 +98,7 @@ const AdminImportTurnus = () => {
                     <option value={turn.id} className="turn__option">{turn.name}</option>
                 ))}
             </select>
-           <p className={"import-arrow"}><FontAwesomeIcon className={"arrow"} icon={"arrow-alt-circle-down"} /> </p>
+            <p className={"import-arrow"}><FontAwesomeIcon className={"arrow"} icon={"arrow-alt-circle-down"}/></p>
             <p>Válassza ki a Turnust ahová szeretné másolni az órákat.:</p>
             <select onChange={selectTurn2} name="turn" id="turner" className="newLesson__turnSelect">
                 {turns.map(turn => (
@@ -60,10 +109,9 @@ const AdminImportTurnus = () => {
         </div>
 
 
-
         <div className="importTurnus__tableContainer">
             <p>Turnusban szereplő órák listája:</p>
-            <select onChange={selectTurn2} name="turn" id="turner" className="newLesson__turnSelect">
+            <select onChange={selectListTurn} name="turn" id="turner" className="newLesson__turnSelect">
                 {turns.map(turn => (
                     <option value={turn.id} className="turn__option">{turn.name}</option>
                 ))}
@@ -78,13 +126,9 @@ const AdminImportTurnus = () => {
                     <th>Dátuma</th>
                 </tr>
                 </thead>
-                <tbody>
-
-                </tbody>
+                {tableBody}
             </table>
         </div>
-
-
 
 
     </div>
--- src/components/importLesson/ImportLesson.js
+++ src/components/importLesson/ImportLesson.js
@@ -1,9 +1,73 @@
 import React from "react";
+import axios from "axios";
+import authHeader from "../../security/auth-header";
+import {useCookies} from "react-cookie";
 
 
 const ImportLesson = (props) => {
+    const url = process.env.REACT_APP_URL;
+    const [cookies, setCookies] = useCookies("user")
+
+
+    function changeDate(e) {
+        console.log(e.target.value)
+        saveNewDate(e.target.value, props.type).then(res => {
+            console.log(res)
+        })
+    }
+
+    /*
+     @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long turnusId;
+
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    private Integer point;
+
+    @Builder.Default
+    private String code = null;
+
+    private Boolean potlas;
+
+    @Builder.Default
+    private Boolean active = true;
+
+    @Builder.Default
+    private Boolean isAttendanceOpen = false;
+     */
+
+    async function saveNewDate(date, type) {
+        let data = {
+            turnusId: props.data.turnusId,
+            name: props.data.name,
+            date: date,
+            point: props.data.point,
+            potlas: props.data.potlas
+        }
+        try {
+            const resp = await axios.put(url + "/" + type + "/" + props.data.id + "/update" , data, {headers: authHeader(cookies.user)} )
+            return resp.data
+        }
+        catch (error) {
+
+        }
+    }
+
+
+
+
+
     return <tr>
-        <td>hi</td>
+        <td>{props?.type}</td>
+        <td>{props?.data.name}</td>
+        <td><input  onChange={changeDate} type="date"
+        defaultValue={props?.data.date}/></td>
     </tr>
 }
 
