d3c361d122e20eb26789406ea10c0e11a70c7cf1
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-03 11:24:12 +0100

    created new page: StudentSettings

---
+++ src/pages/studentSettings/StudentSettings.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const StudentSettings = () => {
+
+
+
+
+    return <div className="studentSettings__container">
+
+    </div>
+
+}
+
+
+
+
+export default StudentSettings;
