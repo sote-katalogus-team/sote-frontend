1b96af2f4967a5d7813e80ffde51371bd1a9492a
maryn1421 <56552980+maryn1421@users.noreply.github.com>
2021-03-03 11:56:18 +0100

    added a base html body to the student settings page

--- src/pages/studentSettings/StudentSettings.js
+++ src/pages/studentSettings/StudentSettings.js
@@ -1,13 +1,78 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import authHeader from "../../security/auth-header";
+import {useCookies} from "react-cookie";
 
 
 const StudentSettings = () => {
+    const [turns, setTurns] = useState([])
+    const url = process.env.REACT_APP_URL;
+    const [cookies, setCookies] = useCookies(["user"])
 
+
+    useEffect(() => {
+        fetchTurns().then(res => {
+            setTurns(res)
+        })
+    }, [])
+
+
+
+    async function fetchTurns() {
+        try {
+            const resp = await axios.get(url + "/turnus/all_by_year", {headers: authHeader(cookies.user)})
+            return resp.data
+        } catch (error) {
+
+        }
+    }
 
 
 
     return <div className="studentSettings__container">
+            <div className="studentSettings__inputContainer">
+                <div className="newName">
+                    <p>Change name:</p>
+                    <form >
+                        <input required={"required"} type="text"/> <br/>
+                        <button  type={"submit"}>save</button>
+                    </form>
+                </div>
+                <div className="newEmail">
+                    <p>Change Email:</p>
+                    <form >
+                    <input  required={"required"} type="email"/> <br/>
+                    <input  required={"required"} type="email"/> <br/>
+                    <button type={"submit"}>save</button>
+                    </form>
+                </div>
+                <div className="newPassword">
+                    <p>Change Password:</p>
+                    <form >
+                        <input placeholder={"•••••••••"}  required={"required"} id={"newPass1"} type="password"/> <br/>
+                        <input placeholder={"•••••••••"}  required={"required"} id={"newPass2"} type="password"/> <br/>
+                        <button type={"submit"}>save</button>
+                    </form>
 
+                </div>
+                <div className="newTurnus">
+                    <p>Change Turnus:</p>
+                    <select name="turn-1" id="turner-1" className="newLesson__turnSelect">
+                        {turns.map(turn => (
+                            <option value={turn.id} className="turn__option">{turn.name}</option>
+                        ))}
+                    </select> <br/>
+                    <button>save</button>
+                </div>
+                <div className="newNeptunCode">
+                    <p>Change neptun code:</p>
+                    <form >
+                        <input  required={"required"} type="text"/> <br/>
+                        <button type={"submit"}>save</button>
+                    </form>
+                </div>
+                <button onClick={e => {window.location = "/student"}}>Back</button>
+            </div>
     </div>
 
 }
